1aadb85116104652994cb3355e598d1875b83b71
Arda Kutlu <ardakutlu@gmail.com>
2022-01-08 10:18:26 +0000

    test user and mockup cleaned up for better testing ground

--- tests/mockup.py
+++ tests/mockup.py
@@ -1,63 +1,70 @@
 """Creates a mockup commons and user folder"""
-import uuid
 import shutil
 import os
 from functools import wraps
 
+
 class Mockup(object):
     def __init__(self):
-        self._original_user_folder = None
-        self._user_folder = None
-        self._common_folder = None
-        # self._salt = str(uuid.uuid4()).split("-")[-1]
-        self._salt = "BCK"
+        self.user_backup_path = os.path.normpath(os.path.join(os.path.expanduser('~'), "TikManager4_BCK"))
+        self.test_project_path = os.path.normpath(os.path.join(os.path.expanduser('~'), "test_project"))
+        self.mockup_commons_path = os.path.normpath(os.path.join(os.path.expanduser('~'), "mockup_common"))
+        self.user_path = os.path.normpath(os.path.join(os.path.expanduser('~'), "TikManager4"))
 
     @property
-    def common_folder(self):
-        return self._common_folder
+    def project(self):
+        return self.test_project_path
+
+    @property
+    def common(self):
+        return self.mockup_commons_path
+
+    @property
+    def user_folder(self):
+        return self.user_path
 
     def prepare(self):
-        self._common_folder = self._create_mockup_common()
-        self._original_user_folder = self._backup_user_folder()
+        self.__create_test_project()
+        self.__create_mockup_common()
+        # self.backup_user()
 
     def revert(self):
-        if self._original_user_folder:
-            shutil.rmtree(self._user_folder)
-            os.rename(self._user_folder.replace("TikManager4", "TikManager4_%s" % self._salt), self._user_folder)
+        if os.path.isdir(self.user_backup_path):
+            if os.path.isdir(self.user_path):
+                shutil.rmtree(self.user_path)
+            os.rename(self.user_backup_path, self.user_path)
 
-    def _create_mockup_common(self):
-        mockup_common = os.path.join(os.path.expanduser("~"), "mockup_common")
-        if os.path.exists(mockup_common):
-            shutil.rmtree(mockup_common)
-        os.mkdir(mockup_common)
-        return mockup_common
+    def __create_test_project(self):
+        if os.path.isdir(self.test_project_path):
+            shutil.rmtree(self.test_project_path)
+        os.mkdir(self.test_project_path)
 
-    def _backup_user_folder(self):
-        self._user_folder = os.path.normpath(os.path.join(os.path.expanduser('~'), "TikManager4"))
-        backup_folder = os.path.join(os.path.expanduser("~"), "TikManager4_%s" % self._salt)
-        if os.path.exists(backup_folder):
-            shutil.rmtree(self._user_folder)
-            return None
-        if os.path.isdir(self._user_folder):
-            return os.rename(self._user_folder, self._user_folder.replace("TikManager4", "TikManager4_%s" % self._salt))
-        return None
+    def __create_mockup_common(self):
+        if os.path.isdir(self.mockup_commons_path):
+            shutil.rmtree(self.mockup_commons_path)
+        os.mkdir(self.mockup_commons_path)
 
-def fresh_start(func):
+    def backup_user(self):
+        if os.path.isdir(self.user_backup_path):
+            shutil.rmtree(self.user_backup_path)
+        if os.path.isdir(self.user_path):
+            os.rename(self.user_path, self.user_backup_path)
+        else:
+            os.makedirs(self.user_path)
+
+def clean_user(func):
     """Decorator to make a fresh start (commons and user folder)
     """
+
     @wraps(func)
     def _fresh(*args, **kwargs):
         m = Mockup()
-        m.prepare()
+        m.backup_user()
         try:
             return func(*args, **kwargs)
         except Exception as e:
             raise
         finally:
             m.revert()
-            print("User folder reverted back")
-    return _fresh
 
-# a = Mockup()
-# a.prepare()
-# a.revert()
+    return _fresh
--- tests/test_user.py
+++ tests/test_user.py
@@ -1,35 +1,108 @@
 """Tests for User related functions"""
-from .mockup import Mockup
-from tik_manager4.objects import user, main
+from .mockup import Mockup, clean_user
+from tik_manager4.objects import user
 
 
 class TestUser:
     mock = Mockup()
     mock.prepare()
-    user.User(commons_directory=mock.common_folder)
-    tik = main.Main()
+    user.User(commons_directory=mock.common)
+    from tik_manager4.objects.main import Main
+    tik = Main()
 
+    @clean_user
+    def test_reinitializing_user(self):
+        """Tests validating the user information (again)"""
+        assert self.tik.user._validate_user_data() == 1, "Existing user data cannot be initialized"
+
+    @clean_user
     def test_query_users(self):
+        """Tests getting the user list from commons database"""
         user_list = self.tik.user.commons.get_users()
         assert user_list
-        self.mock.revert()
 
+    @clean_user
     def test_get_active_user(self):
+        """Tests getting the currently active user from user database"""
         assert self.tik.user.get_active_user() == "Generic"
-        self.mock.revert()
 
+    @clean_user
     def test_authenticating_user(self):
+        """Tests authenticating the active user"""
         assert self.tik.user
         assert self.tik.user.authenticate("1234")
-        self.mock.revert()
 
+    @clean_user
     def test_switching_users(self):
+        """Tests switching between users"""
+        # setting user without password authentication
         assert self.tik.user.set_active_user("Admin") == ("Admin", "Success")
         assert not self.tik.user.is_authenticated
+        # setting user with password authentication
         assert self.tik.user.set_active_user("Generic", password="1234")
         assert self.tik.user.is_authenticated
-        self.mock.revert()
 
+    @clean_user
+    def test_adding_new_users(self):
+        """Tests adding new users to database"""
+        # test adding by users by not permitted users
+        assert self.tik.user.set_active_user("Generic", password="1234")
+        assert self.tik.user.create_new_user("Test_BasicUser", "tbu", "password", 0) == \
+               (-1, 'User Generic has no permission to create new users')
 
+        assert self.tik.user.set_active_user("Admin")  # non-authenticated Admin
+        assert self.tik.user.create_new_user("Test_BasicUser", "tbu", "password", 0) == \
+               (-1, "Active user is not authenticated or the password is wrong")
+        assert self.tik.user.create_new_user("Test_BasicUser", "tbu", "password", 0,
+                                             active_user_password="WRONG_PASS") == \
+               (-1, "Active user is not authenticated or the password is wrong")
+        assert self.tik.user.create_new_user("Test_BasicUser", "tbu", "password", 0,
+                                             active_user_password="1234") == \
+               (1, "Success")
+        assert self.tik.user.create_new_user("Test_TaskUser", "ttu", "password", 1) == (1, 'Success')
+        assert self.tik.user.create_new_user("Test_ProjectUser", "ttu", "password", 2) == (1, 'Success')
+        assert self.tik.user.create_new_user("Test_AdminUser", "ttu", "password", 3) == (1, 'Success')
 
+        assert self.tik.user.create_new_user("Test_BasicUser", "tbu", "password", 0) == \
+               (-1, 'User Test_BasicUser already exists. Aborting')
 
+        assert self.tik.user.set_active_user("Test_AdminUser", password="password") == ("Test_AdminUser", "Success")
+        assert self.tik.user.create_new_user("Extra_User", "ext", "extra", 2) == (1, "Success")
+
+    @clean_user
+    def test_change_user_password(self):
+        # Change active user passes
+        assert self.tik.user.set_active_user("Generic")
+        # test providing wrong password
+        assert self.tik.user.change_user_password("WRONG_PASS", "amazing_password") == (-1, "Old password for Generic does "
+                                                                                        "not match")
+        assert not self.tik.user.is_authenticated
+        assert self.tik.user.authenticate("amazing_password") == (-1, "Wrong password provided for user Generic")
+        assert not self.tik.user.is_authenticated
+        # test correct password
+        assert self.tik.user.change_user_password("1234", "amazing_password") == (1, "Success")
+        assert not self.tik.user.is_authenticated
+        assert self.tik.user.authenticate("amazing_password") == (1, "Success")
+        assert self.tik.user.authenticate("wtf") == (-1, "Wrong password provided for user Generic")
+        assert not self.tik.user.is_authenticated
+
+        # Change other user passes
+        assert self.tik.user.change_user_password("WRONG_PASS", "awesome_password", user_name="Admin") == \
+               (-1, "Old password for Admin does not match")
+        assert self.tik.user.set_active_user("Admin", password="awesome_password")
+        assert self.tik.user.change_user_password("1234", "awesome_password", user_name="Admin") == (1, "Success")
+        assert self.tik.user.set_active_user("Admin", password="awesome_password") == ("Admin", "Success")
+
+    @clean_user
+    def test_delete_user(self):
+        # self.test_adding_new_users()
+        self.tik.user.set_active_user("Test_ProjectUser", password="password")
+        assert self.tik.user.delete_user("Extra_User") == (-1, "User Test_ProjectUser has no permission to delete users")
+        self.tik.user.set_active_user("Test_AdminUser")
+        assert self.tik.user.delete_user("Admin") == (-1, "Active user is not authenticated or the password is wrong")
+        self.tik.user.authenticate("password")
+
+        assert self.tik.user.delete_user("Admin") == (-1, "Admin User cannot be deleted")
+        assert self.tik.user.delete_user("Generic") == (-1, "Generic User cannot be deleted")
+        assert self.tik.user.delete_user("NoOne") == (-1, "User NoOne does not exist. Aborting")
+        assert self.tik.user.delete_user("Extra_User") == (1, "Success")
