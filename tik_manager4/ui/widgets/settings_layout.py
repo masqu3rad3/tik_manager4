7a5eb12fc67730f4cef95707dd47d0c9ea4d9062
Arda Kutlu <ardakutlu@gmail.com>
2022-06-08 23:20:29 +0100

    ends

--- tik_manager4/ui/widgets/settings_layout.py
+++ tik_manager4/ui/widgets/settings_layout.py
@@ -4,7 +4,7 @@ import os
 import sys
 from tik_manager4.core.settings import Settings
 # from PyQt5 import QtWidgets, QtGui, QtCore
-
+#
 from tik_manager4.ui.Qt import QtWidgets, QtGui, QtCore
 
 
@@ -12,10 +12,12 @@ from tik_manager4.ui.Qt import QtWidgets, QtGui, QtCore
 class ValueChangeStr(QtCore.QObject):
     """Simple QObject inheritance to pass the Signal and event to custom widgets"""
     valueChanged = QtCore.Signal(str)
+
     def __init__(self):
         super(ValueChangeStr, self).__init__()
 
     def valueChangeEvent(self, e):
+        print(e)
         self.valueChanged.emit(e)
 
 
@@ -24,6 +26,8 @@ class ValueChangeInt(QtCore.QObject):
     valueChanged = QtCore.Signal(int)
 
     def valueChangeEvent(self, e):
+        print(e)
+
         self.valueChanged.emit(e)
 
 
@@ -32,6 +36,8 @@ class ValueChangeFloat(QtCore.QObject):
     valueChanged = QtCore.Signal(float)
 
     def valueChangeEvent(self, e):
+        print(e)
+
         self.valueChanged.emit(e)
 
 
@@ -40,6 +46,8 @@ class ValueChangeBool(QtCore.QObject):
     valueChanged = QtCore.Signal(bool)
 
     def valueChangeEvent(self, e):
+        print(e)
+
         self.valueChanged.emit(e)
 
 
@@ -47,60 +55,67 @@ class ValueChangeBool(QtCore.QObject):
 class Boolean(QtWidgets.QCheckBox):
     com = ValueChangeBool()
 
-    def __init__(self, value=False, disables=None, **kwargs):
+    def __init__(self, name, value=False, disables=None, **kwargs):
         super(Boolean, self).__init__()
+        self.value = value
+        self.setObjectName(name)
         self.setChecked(value)
         self.stateChanged.connect(self.com.valueChangeEvent)
-        self.disable_value = None
-        self.disable_list = []
-
-    def toggle(self, value, widget):
-        if value == self.disable_value:
-            widget.setEnabled(False)
-        else:
-            widget.setEnabled(True)
+        self.disables = disables or []
 
 
 class String(QtWidgets.QLineEdit):
     com = ValueChangeStr()
 
-    def __init__(self, value="", placeholder="", **kwargs):
+    def __init__(self, name, value="", placeholder="", disables=None, **kwargs):
         super(String, self).__init__()
+        self.value = value
+        self.setObjectName(name)
         self.setText(value)
         self.setPlaceholderText(placeholder)
         self.textEdited.connect(self.com.valueChangeEvent)
+        self.disables = disables or []
 
 
 class Combo(QtWidgets.QComboBox):
     com = ValueChangeInt()
 
-    def __init__(self, value=0, items=None, **kwargs):
+    def __init__(self, name, value=0, items=None, disables=None, **kwargs):
         super(Combo, self).__init__()
+        self.value = value
+        self.setObjectName(name)
         self.addItems(items or [])
         self.setCurrentIndex(value)
         self.currentIndexChanged.connect(self.com.valueChangeEvent)
+        self.disables = disables or []
 
 
 class SpinnerInt(QtWidgets.QSpinBox):
     com = ValueChangeInt()
 
-    def __init__(self, value=0, minimum=-99999, maximum=99999, **kwargs):
+    def __init__(self, name, value=0, minimum=-99999, maximum=99999, disables=None, **kwargs):
         super(SpinnerInt, self).__init__()
+        self.value = value
+        self.setObjectName(name)
         self.setMinimum(minimum)
         self.setMaximum(maximum)
         self.setValue(value)
         self.valueChanged.connect(self.com.valueChangeEvent)
+        self.disables = disables or []
 
 
 class SpinnerFloat(QtWidgets.QDoubleSpinBox):
     com = ValueChangeFloat()
 
-    def __init__(self, value=0, minimum=-99999.9, maximum=99999.9, **kwargs):
+    def __init__(self, name, value=0, minimum=-99999.9, maximum=99999.9, disables=None, **kwargs):
         super(SpinnerFloat, self).__init__()
+        self.value = value
+        self.setObjectName(name)
         self.setMinimum(minimum)
         self.setMaximum(maximum)
         self.setValue(value)
         self.valueChanged.connect(self.com.valueChangeEvent)
+        self.disables = disables or []
 
 
 class SettingsLayout(QtWidgets.QFormLayout):
@@ -118,24 +133,69 @@ class SettingsLayout(QtWidgets.QFormLayout):
 
         self.settings_data = settings_obj
 
-        self.populate()
+        self.widgets = self.populate()
+        self.signal_connections(self.widgets)
 
     def populate(self):
         """Creates the widgets"""
+        _widgets = []
         for name, properties in self.settings_data._currentValue.items():
             _type = properties.get("type", None)
             _label = QtWidgets.QLabel(text=name)
             _widget_class = self.widget_dict.get(_type)
             if not _widget_class:
                 continue
-            self.addRow(_label, _widget_class(**properties))
-
-            _widget_class.com.valueChanged.connect(lambda x: print(x))
+            # properties.update({"objectName": name})
+            # _widget_class.setObjectName(name)
+            _widget = _widget_class(name, **properties)
+            self.addRow(_label, _widget)
+            # _widget_class.com.valueChanged.connect(lambda x: print(x))
             _widget_class.com.valueChanged.connect(lambda x: self.settings_data.edit_property(name, x))
+            _widgets.append(_widget)
+        return _widgets
+        # yield _widget_class
 
-            # _widget_class.com.valueChanged.connect(self.test)
+        # _widget_class.com.valueChanged.connect(self.test)
 
-            # _widget_class.valueChanged.connect(lambda x: print(x))
+        # _widget_class.valueChanged.connect(lambda x: print(x))
+
+    def signal_connections(self, widget_list):
+        """Creates the enable/disable logic between widgets. This needs to be done after population"""
+        for widget in widget_list:
+            # get the disable list
+            for disable_data in widget.disables:
+                # find the target widget / 2nd item in data list is the target
+                # Example data: [false, "testString"]
+                disable_widget = self.__find_widget(disable_data[1], widget_list)
+                print(disable_widget)
+                condition = disable_data[0]
+
+                if not disable_widget:
+                    continue
+                # widget.com.valueChanged.connect(lambda x: disable_widget.setEnabled(False) if (x == condition)
+                # else disable_widget.setEnabled(True))
+                widget.com.valueChanged.connect(lambda x: self.__toggle_enabled(x, condition, disable_widget))
+                self.__toggle_enabled(widget.value, condition, disable_widget)
+                # print(widget.objectName())
+
+            # print(widget.objectName())
+
+    def __toggle_enabled(self, value, condition, widget):
+        """Disables the widget if value equals to condition. Else enables"""
+        # print(value, condition)
+        if value == condition:
+            widget.setEnabled(False)
+        else:
+            widget.setEnabled(True)
+
+    def __find_widget(self, object_name, widget_list):
+        """Finds the widget by given object name inside the widget list"""
+        for widget in widget_list:
+            # print(widget.objectName(), object_name)
+            if widget.objectName() == object_name:
+
+                return widget
+        return None
 
 
 def main():
