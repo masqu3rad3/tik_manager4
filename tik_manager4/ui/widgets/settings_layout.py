4e4a7a3040157987a50c3b3bdb897376376e5a33
Arda Kutlu <Arda.Kutlu@Rebellion.co.uk>
2022-06-08 10:00:01 +0100

    ends

--- tik_manager4/ui/widgets/settings_layout.py
+++ tik_manager4/ui/widgets/settings_layout.py
@@ -3,51 +3,86 @@
 import os
 import sys
 from tik_manager4.core.settings import Settings
-from PyQt5 import QtWidgets, QtGui, QtCore
-from PyQt5.QtCore import pyqtSignal as Signal
-# from tik_manager4.ui.Qt import QtWidgets, QtGui, QtCore
+# from PyQt5 import QtWidgets, QtGui, QtCore
+
+from tik_manager4.ui.Qt import QtWidgets, QtGui, QtCore
+
+
+# Signal - Slot requires QObject inheritance
+class ValueChangeStr(QtCore.QObject):
+    """Simple QObject inheritance to pass the Signal and event to custom widgets"""
+    valueChanged = QtCore.Signal(str)
+
+    def valueChangeEvent(self, e):
+        self.valueChanged.emit(e)
+
+
+class ValueChangeInt(QtCore.QObject):
+    """Simple QObject inheritance to pass the Signal and event to custom widgets"""
+    valueChanged = QtCore.Signal(int)
+
+    def valueChangeEvent(self, e):
+        self.valueChanged.emit(e)
+
+class ValueChangeFloat(QtCore.QObject):
+    """Simple QObject inheritance to pass the Signal and event to custom widgets"""
+    valueChanged = QtCore.Signal(float)
+
+    def valueChangeEvent(self, e):
+        self.valueChanged.emit(e)
+
+
+class ValueChangeBool(QtCore.QObject):
+    """Simple QObject inheritance to pass the Signal and event to custom widgets"""
+    valueChanged = QtCore.Signal(bool)
+
+    def valueChangeEvent(self, e):
+        self.valueChanged.emit(e)
 
 class Boolean(QtWidgets.QCheckBox):
-    # valueChanged = Signal(str)
-
+    com = ValueChangeBool()
     def __init__(self, value=False, *args, **kwargs):
         super(Boolean, self).__init__()
-        # super(Boolean, self).__init__()
         self.setChecked(value)
-        # self.stateChanged.connect(self.valueChangeEvent)
-
-    # def valueChangeEvent(self, e):
-    #     self.valueChanged.emit(e)
+        self.stateChanged.connect(self.com.valueChangeEvent)
 
 
 class String(QtWidgets.QLineEdit):
+    com = ValueChangeStr()
     def __init__(self, value="", placeholder="", **kwargs):
         super(String, self).__init__()
         self.setText(value)
         self.setPlaceholderText(placeholder)
+        self.textEdited.connect(self.com.valueChangeEvent)
 
 
 class Combo(QtWidgets.QComboBox):
+    com = ValueChangeInt()
     def __init__(self, value=0, items=None, **kwargs):
         super(Combo, self).__init__()
         self.addItems(items or [])
         self.setCurrentIndex(value)
+        self.currentIndexChanged.connect(self.com.valueChangeEvent)
 
 
 class SpinnerInt(QtWidgets.QSpinBox):
+    com = ValueChangeInt()
     def __init__(self, value=0, minimum=-99999, maximum=99999, **kwargs):
         super(SpinnerInt, self).__init__()
         self.setMinimum(minimum)
         self.setMaximum(maximum)
         self.setValue(value)
+        self.valueChanged.connect(self.com.valueChangeEvent)
 
 
 class SpinnerFloat(QtWidgets.QDoubleSpinBox):
+    com = ValueChangeFloat()
     def __init__(self, value=0, minimum=-99999.9, maximum=99999.9, **kwargs):
         super(SpinnerFloat, self).__init__()
         self.setMinimum(minimum)
         self.setMaximum(maximum)
         self.setValue(value)
+        self.valueChanged.connect(self.com.valueChangeEvent)
 
 
 class SettingsLayout(QtWidgets.QFormLayout):
@@ -76,7 +111,8 @@ class SettingsLayout(QtWidgets.QFormLayout):
                 continue
             self.addRow(_label, _widget_class(**properties))
 
-            _widget_class.valueChanged.connect(lambda x: print(x))
+            _widget_class.com.valueChanged.connect(lambda x: print(x))
+            # _widget_class.valueChanged.connect(lambda x: print(x))
 
 
 if __name__ == '__main__':
