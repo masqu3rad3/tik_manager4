7cbb6d330a61bf11231e851abed58d391a235533
Arda Kutlu <ardakutlu@gmail.com>
2023-01-21 11:49:13 +0000

    settings_layout feed separated to ui_definition and settings_data. new_subproject dialog adapted to that. Next new_task dialog.

--- tik_manager4/ui/dialog/new_subproject.py
+++ tik_manager4/ui/dialog/new_subproject.py
@@ -20,100 +20,176 @@ class NewSubproject(QtWidgets.QDialog):
         self._parent_sub = parent_sub or project_object
         self.parent = parent
         self._feedback = feedback.Feedback(parent=self)
-        self.settings = Settings()
         self.setWindowTitle("New Subproject")
         # self.setWindowFlags(Qt.WindowStaysOnTopHint)
         self.setFixedSize(600, 400)
         self.setModal(True)
-        self.populate_settings()
+        self.ui_definition = self.define_ui_dictionary()
         self._init_ui()
 
         self._new_subproject = None
 
-    def populate_settings(self):
-        print(self._parent_sub)
-        self.settings.add_property("name", {
-            "display_name": "Name :",
-            "type": "validatedString",
-            "value": "",
-        })
-        self.settings.add_property("path", {
-            "display_name": "Path :",
-            "type": "string",
-            "value": self._parent_sub.path,
-        })
-        # add a property for each inherited metadata
-        # for key, metaitem in self._parent_sub.metadata.items():
-        #     print("--------")
-        #     print(key)
-        #     print(type(metaitem.value).__name__)
-        #     print("--------")
-        #     if metaitem.value:
-        #         self.settings.add_property(key, {
-        #             "display_name": "{} :".format(key),
-        #             "type": type(metaitem.value).__name__,
-        #             "value": metaitem.value,
-        #         })
+    def define_ui_dictionary(self):
 
+        # TODO resolution fallback need to be implemented and not hard coded
         _resolution = self._parent_sub.metadata.get_value("resolution", [1920, 1080])
-        self.settings.add_property("resolution_override", {
-            "display_name": "Override Resolution :",
-            "type": "multi",
-            "value": {
-                "override": {
-                    "type": "boolean",
-                    "value": False,
-                    "disables": [[False, "resolutionX"], [False, "resolutionY"]]
+
+        # TODO settings data will be flat and will be created from the _ui_definition.
+        # TODO This means everything under 'multi' type keys will moved into same level
+        # TODO We need to make sure that the sub-keys under 'value' in 'multi' type are unique
+        _ui_definition = {
+            "name":
+               {
+                   "display_name": "Name :",
+                   "type": "validatedString",
+                   "value": "",
+               },
+            "path":
+               {
+                   "display_name": "Path :",
+                   "type": "string",
+                   "value": self._parent_sub.path,
+               },
+            "resolution_override":
+                {
+                    "display_name": "Override Resolution :",
+                    "type": "multi",
+                    "value": {
+                        "overrideResolution": {
+                            "type": "boolean",
+                            "value": False,
+                            "disables": [[False, "resolutionX"], [False, "resolutionY"]]
+                        },
+                        "resolutionX": {
+                            "type": "spinnerInt",
+                            "value": _resolution[0],
+                        },
+                        "resolutionY": {
+                            "type": "spinnerInt",
+                            "value": _resolution[1],
+                        }
+                    }
                 },
-                "resolutionX": {
-                    "type": "spinnerInt",
-                    "value": _resolution[0],
+            "fps_override":
+                {
+                    "display_name": "FPS Override :",
+                    "type": "multi",
+                    "value": {
+                        "overrideFPS": {
+                            "type": "boolean",
+                            "value": False,
+                            "disables": [[False, "fps"]]
+                        },
+                        "fps": {
+                            "type": "spinnerInt",
+                            "object_name": "fps",
+                            "value": self._parent_sub.metadata.get_value("fps", 24),
+                        }
+                    }
                 },
-                "resolutionY": {
-                    "type": "spinnerInt",
-                    "value": _resolution[1],
+            "mode_override":
+                {
+                    "display_name": "Mode Override :",
+                    "type": "multi",
+                    "value": {
+                        "overrideMode": {
+                            "type": "boolean",
+                            "value": False,
+                            "disables": [[False, "mode"]]
+                        },
+                        "mode": {
+                            "type": "combo",
+                            "object_name": "mode",
+                            "items": ["", "asset", "shot"],
+                            "value": self._parent_sub.metadata.get_value("mode") or "",
+                        }
+                    }
                 }
-            }
-        })
-        self.settings.add_property("fps_override", {
-            "display_name": "FPS Override :",
-            "type": "multi",
-            "value": {
-                "override": {
-                    "type": "boolean",
-                    "value": False,
-                    "disables": [[False, "fps"]]
-                },
-                "fps": {
-                    "type": "spinnerInt",
-                    "object_name": "fps",
-                    "value": self._parent_sub.metadata.get_value("fps", 24),
-                }
-            }
-        })
-        self.settings.add_property("mode_override", {
-            "display_name": "Mode Override :",
-            "type": "multi",
-            "value": {
-                "override": {
-                    "type": "boolean",
-                    "value": False,
-                    "disables": [[False, "mode"]]
-                },
-                "mode": {
-                    "type": "combo",
-                    "object_name": "mode",
-                    "items": ["", "asset", "shot"],
-                    "value": self._parent_sub.metadata.get_value("mode") or "",
-                }
-            }
-        })
+        }
+
+        return _ui_definition
+
+    # def define_initial_settings_data(self):
+    #     """Create a flat dictionary by grabbing the values from the ui_definition."""
+    #
+    #     _settings_data = Settings()
+    #     for _key, _value in self.ui_definition.items():
+    #         _settings_data.set_value(_key, _value["value"])
+
+
+
+        # self.ui_definition.add_property("name", {
+        #     "display_name": "Name :",
+        #     "type": "validatedString",
+        #     "value": "",
+        # })
+        # self.ui_definition.add_property("path", {
+        #     "display_name": "Path :",
+        #     "type": "string",
+        #     "value": self._parent_sub.path,
+        # })
+
+
+        # _resolution = self._parent_sub.metadata.get_value("resolution", [1920, 1080])
+        # self.ui_definition.add_property("resolution_override", {
+        #     "display_name": "Override Resolution :",
+        #     "type": "multi",
+        #     "value": {
+        #         "override": {
+        #             "type": "boolean",
+        #             "value": False,
+        #             "disables": [[False, "resolutionX"], [False, "resolutionY"]]
+        #         },
+        #         "resolutionX": {
+        #             "type": "spinnerInt",
+        #             "value": _resolution[0],
+        #         },
+        #         "resolutionY": {
+        #             "type": "spinnerInt",
+        #             "value": _resolution[1],
+        #         }
+        #     }
+        # })
+        # self.ui_definition.add_property("fps_override", {
+        #     "display_name": "FPS Override :",
+        #     "type": "multi",
+        #     "value": {
+        #         "override": {
+        #             "type": "boolean",
+        #             "value": False,
+        #             "disables": [[False, "fps"]]
+        #         },
+        #         "fps": {
+        #             "type": "spinnerInt",
+        #             "object_name": "fps",
+        #             "value": self._parent_sub.metadata.get_value("fps", 24),
+        #         }
+        #     }
+        # })
+        # self.ui_definition.add_property("mode_override", {
+        #     "display_name": "Mode Override :",
+        #     "type": "multi",
+        #     "value": {
+        #         "override": {
+        #             "type": "boolean",
+        #             "value": False,
+        #             "disables": [[False, "mode"]]
+        #         },
+        #         "mode": {
+        #             "type": "combo",
+        #             "object_name": "mode",
+        #             "items": ["", "asset", "shot"],
+        #             "value": self._parent_sub.metadata.get_value("mode") or "",
+        #         }
+        #     }
+        # })
 
     def _init_ui(self):
 
         self.main_layout = QtWidgets.QVBoxLayout(self)
 
-        self.settings_layout = SettingsLayout(self.settings, parent=self)
+        self.settings_data = Settings()
+        self.settings_layout = SettingsLayout(self.ui_definition, self.settings_data, parent=self)
         self.main_layout.addLayout(self.settings_layout)
 
         # create a button box
@@ -130,26 +206,26 @@ class NewSubproject(QtWidgets.QDialog):
         self.button_box.rejected.connect(self.reject)
 
     def on_create_subproject(self):
-        name = self.settings.get_property("name")["value"]
-        path = self.settings.get_property("path")["value"]
+        name = self.settings_data.get_property("name")
+        path = self.settings_data.get_property("path")
 
-        resolution_override = self.settings.get_sub_property(["resolution_override", "value", "override", "value"])
+        resolution_override = self.settings_data.get_property("overrideResolution")
         if resolution_override:
-            _resolution_x = self.settings.get_sub_property(["resolution_override", "value", "resolutionX", "value"])
-            _resolution_y = self.settings.get_sub_property(["resolution_override", "value", "resolutionY", "value"])
+            _resolution_x = self.settings_data.get_property("resolutionX")
+            _resolution_y = self.settings_data.get_property("resolutionY")
             resolution = [_resolution_x, _resolution_y]
         else:
             resolution = None
 
-        fps_override = self.settings.get_sub_property(["fps_override", "value", "override", "value"])
+        fps_override = self.settings_data.get_property("overrideFPS")
         if fps_override:
-            fps = self.settings.get_sub_property(["fps_override", "value", "fps", "value"])
+            fps = self.settings_data.get_property("fps")
         else:
             fps = None
 
-        mode_override = self.settings.get_sub_property(["mode_override", "value", "override", "value"])
+        mode_override = self.settings_data.get_property("overrideMode")
         if mode_override:
-            mode = self.settings.get_sub_property(["mode_override", "value", "mode", "value"])
+            mode = self.settings_data.get_property("mode")
         else:
             mode = None
 
