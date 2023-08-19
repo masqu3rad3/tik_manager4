5318480222078fca91ab829609a5bca4e387ae2d
Caglar Kutlu <caglar.kutlu@gmail.com>
2023-08-19 22:43:37 +0200

    bring get_files coverage back

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,6 +2,7 @@
 import os
 import glob
 import pytest
+import platform
 from pathlib import Path
 from tik_manager4.core import filelog
 from tik_manager4.core import io
@@ -37,6 +38,13 @@ def test_filelog():
 
     log_file_contents_truth = "============\nnew_log_name\n============\n\n"
     assert log_file_contents == log_file_contents_truth
+    
+    bytes_ = codecs.encode(log_file_contents_truth)
+    newline_count = bytes_.count(b"\n")
+    nbytes = len(bytes_)
+    nbytes_truth_per_system = {"Linux": nbytes, "Windows": nbytes + newline_count}
+
+    assert log.get_size() == nbytes_truth_per_system(platform.system())
 
 
 def test_io():
