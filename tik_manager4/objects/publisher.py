05796ca713c8132bb941c1faa9321aca9318226d
Arda Kutlu <ardakutlu@gmail.com>
2023-11-04 18:04:23 +0000

    ends

--- tik_manager4/objects/publisher.py
+++ tik_manager4/objects/publisher.py
@@ -2,11 +2,14 @@
 # pylint: disable=consider-using-f-string
 """Publisher Handler."""
 
+import pathlib
+
 from tik_manager4.core import filelog
 
 LOG = filelog.Filelog(logname=__name__, filename="tik_manager4")
 
 from tik_manager4 import dcc
+from tik_manager4.objects.publish import Publish
 
 class Publisher():
     _dcc_handler = dcc.Dcc()
@@ -15,9 +18,15 @@ class Publisher():
         """Initialize the Publisher object."""
         self._work_object = work_object
 
+        # resolved variables
         self._resolved_extracts = {}
         self._resolved_validations = {}
+        self._abs_publish_data_folder = None
+        self._publish_file_name = None
+
         # classs variables
+
+        self._published_object = None
 
     def resolve(self):
         """Resolve the validations, extracts, variables, etc."""
@@ -37,12 +46,30 @@ class Publisher():
             else:
                 LOG.warning("Validation {0} defined in category settings but it is not available on {1}".format(validation, self._dcc_handler.name))
 
+        # resolve the publish data path
+        self._abs_publish_data_folder = self._work_object.get_abs_database_path("publish")
+
+        # get the highest version from the _work_objects publishes
+        # TODO - this is not working
+        # _publishes_versions = list(self._work_object.publishes.values())
+        # latest_publish_version = max(_publishes_versions) if _publishes_versions else 0
+        # self._publish_file_name = f"{self._work_object.name}_v{latest_publish_version+1:03d}.tpub"
+
+
+
     def reserve(self):
         """Reserve the slot for publish.
 
         Makes sure that the publish is not overriden by other users during the process.
         """
-        pass
+        # make sure the publish file is not already exists
+        _publish_file_path = pathlib.Path(self._abs_publish_data_folder) / self._publish_file_name
+        if _publish_file_path.exists():
+            raise ValueError(f"Publish file already exists. {_publish_file_path}")
+
+        # add the publish to the work object
+        self._work_object
+
 
     def validate(self):
         """Validate the scene."""
