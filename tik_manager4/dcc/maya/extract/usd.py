cd5d675166b23ba70be69e3141aa8f3fe6510cb9
Arda Kutlu <ardakutlu@gmail.com>
2023-12-18 17:17:21 +0000

    ends

--- tik_manager4/dcc/maya/extract/usd.py
+++ tik_manager4/dcc/maya/extract/usd.py
@@ -5,10 +5,12 @@ from maya import OpenMaya as om
 from tik_manager4.dcc.extract_core import ExtractCore
 from tik_manager4.dcc.maya import utils
 
+
 # The Collector will only collect classes inherit ExtractCore
 class Usd(ExtractCore):
     """Extract Alembic from Maya scene"""
-    name = "usd" # IMPORTANT. Must match to the one in category_definitions.json
+
+    name = "usd"  # IMPORTANT. Must match to the one in category_definitions.json
     nice_name = "USD"
     color = (71, 143, 203)
     _ranges = utils.get_ranges()
@@ -22,6 +24,7 @@ class Usd(ExtractCore):
             "staticSingleSample": 0,
         }
     }
+
     def __init__(self):
         super(Usd, self).__init__()
         if not cmds.pluginInfo("mayaUsdPlugin", loaded=True, query=True):
@@ -35,45 +38,30 @@ class Usd(ExtractCore):
 
         self._extension = ".usd"
         # Category names must match to the ones in category_definitions.json (case sensitive)
-        self.category_functions = {"Model": self._extract_model,
-                                   "Animation": self._extract_animation,
-                                   "Fx": self._extract_fx}
+        self.category_functions = {
+            "Model": self._extract_model,
+            "LookDev": self._extract_model,
+            "Layout": self._extract_layout,
+            "Assembly": self._extract_model,
+            "Animation": self._extract_animation,
+            "Fx": self._extract_fx,
+            "Lighting": self._extract_lighting,
+
+        }
 
     def _extract_model(self):
         """Extract method for model category"""
         _file_path = self.resolve_output()
         settings = self.settings.get("Animation", {})
-        frame_range = settings.get_property("frame_range")
-        step = settings.get_property("step")
         _options = ";exportUVs=1;exportSkels=none;exportSkin=none;exportBlendShapes=0;exportDisplayColor=0;;exportColorSets=1;exportComponentTags=1;defaultMeshScheme=catmullClark;animation=0;eulerFilter=0;staticSingleSample=0;startTime=1;endTime=1;frameStride=1;frameSample=0.0;defaultUSDFormat=usdc;parentScope=;shadingMode=useRegistry;convertMaterialsTo=[UsdPreviewSurface,MaterialX];exportInstances=1;exportVisibility=1;mergeTransformAndShape=1;stripNamespaces=0;materialsScopeName=mtl"
-        cmds.file(_file_path, force=True, type="USD Export", prompt=False, exportAll=True, options=_options)
-        # cmds.mayaUSDExport(
-        #     file=_file_path,
-        #     exportUVs=True,
-        #     exportSkels=None,
-        #     exportSkin=None,
-        #     exportBlendShapes=False,
-        #     exportDisplayColor=False,
-        #     exportColorSets=True,
-        #     exportComponentTags=True,
-        #     defaultMeshScheme="catmullClark",
-        #     animation=False,
-        #     eulerFilter=False,
-        #     staticSingleSample=False,
-        #     startTime=1,
-        #     endTime=1,
-        #     frameStride=1,
-        #     frameSample=0.0,
-        #     defaultUSDFormat="usdc",
-        #     parentScope="",
-        #     shadingMode="useRegistry",
-        #     convertMaterialsTo=["UsdPreviewSurface", "MaterialX"],
-        #     exportInstances=True,
-        #     exportVisibility=True,
-        #     mergeTransformAndShape=True,
-        #     stripNamespaces=False,
-        #     materialsScopeName="mtl"
-        # )
+        cmds.file(
+            _file_path,
+            force=True,
+            type="USD Export",
+            prompt=False,
+            exportAll=True,
+            options=_options,
+        )
 
     def _extract_animation(self):
         """Extract method for animation category"""
@@ -86,11 +74,28 @@ class Usd(ExtractCore):
         euler_filter = settings.get_property("eulerFilter")
         static_single_sample = settings.get_property("staticSingleSample")
         _options = f";exportUVs=1;exportSkels=none;exportSkin=none;exportBlendShapes=0;exportDisplayColor=0;;exportColorSets=1;exportComponentTags=1;defaultMeshScheme=catmullClark;animation=1;eulerFilter={euler_filter};staticSingleSample={static_single_sample};startTime={start_time};endTime={end_time};frameStride={frame_stride};frameSample={frame_sample};defaultUSDFormat=usdc;parentScope=;shadingMode=useRegistry;convertMaterialsTo=[UsdPreviewSurface,MaterialX];exportInstances=1;exportVisibility=1;mergeTransformAndShape=1;stripNamespaces=0;materialsScopeName=mtl"
-        cmds.file(_file_path, force=True, type="USD Export", prompt=False, exportAll=True, options=_options)
+        cmds.file(
+            _file_path,
+            force=True,
+            type="USD Export",
+            prompt=False,
+            exportAll=True,
+            options=_options,
+        )
 
     def _extract_fx(self):
         """Extract method for fx category"""
         om.MGlobal.displayWarning("Fx category is not implemented yet")
+        pass
+
+    def _extract_lighting(self):
+        """Extract method for lighting category"""
+        om.MGlobal.displayWarning("Lighting category is not implemented yet")
+        pass
+
+    def _extract_layout(self):
+        """Extract method for layout category"""
+        om.MGlobal.displayWarning("Layout category is not implemented yet")
         pass
 
     def _extract_default(self):
--- tik_manager4/dcc/maya/ingest/usd.py
+++ tik_manager4/dcc/maya/ingest/usd.py
@@ -1,9 +1,12 @@
 """Ingest USD."""
 
-from pathlib import Path
+import logging
+
 from maya import cmds
 from maya import OpenMaya as om
 from tik_manager4.dcc.ingest_core import IngestCore
+
+LOG = logging.getLogger(__name__)
 
 class USD(IngestCore):
     """Ingest USD."""
@@ -21,12 +24,21 @@ class USD(IngestCore):
                 raise exc
 
         self.category_functions = {"Model": self._bring_in_model,
-                                   "Animation": self._bring_in_animation}
+                                   "Animation": self._bring_in_animation,
+                                   "Fx": self._bring_in_fx,
+                                   "LookDev": self._bring_in_lookdev,
+                                   "Layout": self._bring_in_layout,
+                                   "Assembly": self._bring_in_assembly,
+                                   }
 
     def _bring_in_model(self, file_path):
         """Import USD File."""
-        cmds.mayaUSDImport(file_path, shadingMode="none")
+        LOG.warning("USD importer for model category is not implemented yet.")
 
     def _bring_in_animation(self, file_path):
         """Import USD File."""
-        cmds.mayaUSDImport(file_path, preferredMaterial="usdPreviewSurface", readAnimData=1)
+        LOG.warning("USD importer for animation category is not implemented yet.")
+
+    def _bring_in_fx(self, file_path):
+        """Import USD File."""
+        LOG.warning("USD importer for fx category is not implemented yet.")
