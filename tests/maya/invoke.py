26053999d8ad3bc254f72e573888b8d2f31ef574
Arda Kutlu <ardakutlu@gmail.com>
2023-12-20 02:11:33 +0000

    ends

--- tests/maya/invoke.py
+++ tests/maya/invoke.py
@@ -3,4 +3,4 @@ import sys
 from pytest import main
 if __name__ == '__main__':
     sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
-    sys.exit(main())
+    sys.exit(main(["./tests/maya"]))
