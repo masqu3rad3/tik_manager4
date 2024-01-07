326104b6278591766ee685a4689a05d90f60461e
Arda Kutlu <ardakutlu@gmail.com>
2024-01-07 20:23:16 +0000

    ends

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -2,6 +2,7 @@
 from pathlib import Path
 import shutil
 import pytest
+import sys
 from tik_manager4.core import utils
 
 import tik_manager4
@@ -21,12 +22,23 @@ class TestUI:
 
 
     def test_launch_main_ui(self, qtbot):
-        with pytest.raises(ImportError):
-            m = main.launch(dcc="Maya")
         m = main.launch(dcc="Standalone")
         qtbot.addWidget(m)
         assert m.windowTitle() == main.WINDOW_NAME
         assert m.objectName() == main.WINDOW_NAME
+
+    def test_launch_ui_with_wrong_dcc(self, qtbot):
+        # make sure all the modules are reloaded
+        kill_list = []
+        for name, _module in sys.modules.items():
+            if name.startswith("tik_manager4"):
+                kill_list.append(name)
+        for x in kill_list:
+            sys.modules.pop(x)
+
+        with pytest.raises(ImportError) as e_info:
+            main.launch(dcc="Maya")
+            assert e_info == "cannot import name 'cmds' from 'maya' (unknown location)"
 
     def test_launch_ui_manually(self, qtbot, main_object):
         parent = main_object.dcc.get_main_window()
