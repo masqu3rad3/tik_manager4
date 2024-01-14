2933e93c96663cece2ea7235288abafbf6797ad2
Arda Kutlu <ardakutlu@gmail.com>
2024-01-14 21:49:46 +0000

    ends

--- tests/test_core.py
+++ tests/test_core.py
@@ -189,7 +189,7 @@ def test_reading_back_from_an_existing_settings_file(tmp_path):
     # modify the first settings file and check if the second one will understand that its modified
     _settings_1.edit_property("test_string", "test_edited")
     _settings_1.apply_settings()
-    assert _settings_2.is_modified() == True
+    assert _settings_2.is_modified()
 
     # _setting_2 should still hold the old data
     assert _settings_2.get_property("test_string") == "test"
