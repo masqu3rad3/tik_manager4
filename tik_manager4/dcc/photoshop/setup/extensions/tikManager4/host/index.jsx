994805065d95df40c43b63584d3855e26facd22d
Arda Kutlu <ardakutlu@gmail.com>
2024-02-03 13:14:52 +0000

    adding the dependencies

--- tik_manager4/dcc/photoshop/setup/extensions/tikManager4/host/index.jsx
+++ tik_manager4/dcc/photoshop/setup/extensions/tikManager4/host/index.jsx
@@ -1,12 +1,12 @@
 function tikUI(){
-    var bat = new File("D://dev//tik_manager4//tik_manager4//dist//tik4//tik4_photoshop.exe");
+    var bat = new File("PATH//TO//PARENT//FOLDER//OF//TIKMANAGER4//tik_manager4//dist//tik4//tik4_photoshop.exe");
     bat.execute();
 }
 function tikSaveVersion(){
-    var bat = new File("D://dev//tik_manager4//tik_manager4//dist//tik4//tik4_ps_new_version.exe");
+    var bat = new File("PATH//TO//PARENT//FOLDER//OF//TIKMANAGER4//tik_manager4//dist//tik4//tik4_ps_new_version.exe");
     bat.execute();
 }
 function tikPublish(){
-    var bat = new File("D://dev//tik_manager4//tik_manager4//dist//tik4//tik4_ps_publish.exe");
+    var bat = new File("PATH//TO//PARENT//FOLDER//OF//TIKMANAGER4//tik_manager4//dist//tik4//tik4_ps_publish.exe");
     bat.execute();
 }
