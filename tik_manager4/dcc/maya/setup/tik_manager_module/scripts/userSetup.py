1ecbf7461f28380f7e71357c028b2fb719557427
Arda Kutlu <ardakutlu@gmail.com>
2024-02-08 01:20:02 +0000

    hotfix - bug fixed which prevents the tik manager4 shelf appear on initial drag and drop setup.

--- tik_manager4/dcc/maya/setup/tik_manager_module/scripts/userSetup.py
+++ tik_manager4/dcc/maya/setup/tik_manager_module/scripts/userSetup.py
@@ -2,4 +2,5 @@ from maya import cmds
 import tik_manager_setup
 
 cmds.evalDeferred(tik_manager_setup.add_python_path)
+cmds.evalDeferred(tik_manager_setup.load_shelves)
 cmds.evalDeferred(tik_manager_setup.load_menu)
