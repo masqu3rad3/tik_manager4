4e1667a840333c61775e9ca06b419513b08abd0a
Arda Kutlu <ardakutlu@gmail.com>
2024-07-07 16:31:39 +0100

    adding fileseq as external source

---
+++ tik_manager4/external/fileseq/__init__.py
@@ -0,0 +1,186 @@
+"""
+A Python library for parsing frame ranges and file sequences commonly
+used in VFX and Animation applications.
+
+The MIT License (MIT)
+
+Original work Copyright (c) 2015 Matthew Chambers
+
+Modified work Copyright 2015 Justin Israel
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+**Frame Range Shorthand**
+
+Support for:
+
+* Standard: 1-10
+* Comma Delimited: 1-10,10-20
+* Chunked: 1-100x5
+* Filled: 1-100y5
+* Staggered: 1-100:3 (1-100x3, 1-100x2, 1-100)
+* Negative frame numbers: -10-100
+* Subframes: 1001-1066x0.25
+* Padding: #=4 padded, @=single pad
+* Printf Syntax Padding: %04d=4 padded, %01d=1 padded
+* Houdini Syntax Padding: $F4=4 padding, $F=1 padded
+* Udim Syntax Padding: <UDIM> or %(UDIM)d, always 4 padded
+
+**FrameSets**
+
+A :class:`~fileseq.frameset.FrameSet` wraps a sequence of frames in a list list container.
+
+Iterate a FrameSet
+
+.. doctest::
+
+    >>> fs = FrameSet("1-5")
+    >>> for f in fs:
+    ...     print(f)
+    1
+    2
+    3
+    4
+    5
+
+Access Frames Using Indices
+
+.. doctest::
+
+    >>> fs = FrameSet("1-100:8")
+    >>> fs[0] # First frame.
+    1
+    >>> fs[-1] # Last frame.
+    98
+
+Access Frames Using Convenience Methods:
+
+.. doctest::
+
+    >>> fs = FrameSet("1-100:8")
+    >>> fs.start() # First frame.
+    1
+    >>> fs.end() # Last frame.
+    98
+
+**FileSequence**
+
+A :class:`~fileseq.filesequence.FileSequence` is a container representing a filepath over a range of frames
+
+Instantiate from string
+
+.. doctest::
+
+    >>> FileSequence("/foo/bar.1-10#.exr")
+    <FileSequence: '/foo/bar.1-10#.exr'>
+
+Format Path for VFX Software
+
+*Using FileSequence.format Method*
+
+.. doctest::
+
+    >>> seq = FileSequence("/foo/bar.1-10#.exr")
+    >>> seq.format(template='{dirname}{basename}{padding}{extension}')
+    '/foo/bar.#.exr'
+    >>> seq = FileSequence("/foo/bar.1-10#.#.exr", allow_subframes=True)
+    >>> seq.format(template='{dirname}{basename}{padding}{extension}')
+    '/foo/bar.#.#.exr'
+
+*Joining*
+
+.. doctest::
+
+    >>> seq = FileSequence("/foo/bar.1-10#.exr")
+    >>> seq.setPadding('%02d')
+    >>> seq
+    <FileSequence: '/foo/bar.1-10%02d.exr'>
+    >>> seq.format(template='{dirname}{basename}{padding}{extension}')
+    '/foo/bar.%02d.exr'
+
+*Get List of File Paths*
+
+.. doctest::
+
+    >>> seq = FileSequence("/foo/bar.1-5#.exr")
+    >>> list(seq)
+    ['/foo/bar.0001.exr',
+     '/foo/bar.0002.exr',
+     '/foo/bar.0003.exr',
+     '/foo/bar.0004.exr',
+     '/foo/bar.0005.exr']
+    >>> [seq[idx] for idx, fr in enumerate(seq.frameSet())]
+    ['/foo/bar.0001.exr',
+     '/foo/bar.0002.exr',
+     '/foo/bar.0003.exr',
+     '/foo/bar.0004.exr',
+     '/foo/bar.0005.exr']
+
+**Finding Sequences on Disk**
+
+Check a Directory for All Existing Sequences
+
+.. doctest::
+
+    >>> seqs = findSequencesOnDisk("/show/shot/renders/bty_foo/v1")
+
+Check a Directory for One Existing Sequence
+
+* Use a '@' or '#' where you might expect to use '*' for a wildcard character.
+* For this method, it doesn't matter how many instances of the padding character
+  you use, it will still find your sequence (unless enabling strict padding option).
+
+Yes:
+
+.. code-block:: python
+
+    findSequenceOnDisk('/foo/bar.@.exr')
+
+Yes:
+
+.. code-block:: python
+
+    findSequenceOnDisk('/foo/bar.@@@@@.exr')
+
+No:
+
+.. code-block:: python
+
+    findSequenceOnDisk('/foo/bar.*.exr')
+
+* To find subframe sequences you must explicitly opt-in
+
+.. code-block:: python
+
+    fileseq.findSequenceOnDisk('/foo/bar.#.#.exr', allow_subframes=True)
+"""
+
+from .constants import PAD_STYLE_DEFAULT, PAD_STYLE_HASH1, PAD_STYLE_HASH4
+from .exceptions import ParseException, MaxSizeException, FileSeqException
+from .frameset import FrameSet
+from .filesequence import FileSequence
+
+padFrameRange = FrameSet.padFrameRange
+framesToFrameRange = FrameSet.framesToFrameRange
+
+getPaddingChars = FileSequence.getPaddingChars
+getPaddingNum = FileSequence.getPaddingNum
+findSequenceOnDisk = FileSequence.findSequenceOnDisk
+findSequencesOnDisk = FileSequence.findSequencesOnDisk
+findSequencesInList = FileSequence.findSequencesInList
---
+++ tik_manager4/external/fileseq/constants.py
@@ -0,0 +1,213 @@
+"""
+constants - General constants of use to fileseq operations.
+"""
+
+import re
+import typing
+
+# The max frame count of a FrameSet before a MaxSizeException
+# exception is raised
+MAX_FRAME_SIZE = 10000000
+
+
+class _PadStyle(object):
+    def __init__(self, name: str):
+        self.__name = name
+
+    def __hash__(self) -> int:
+        return hash(str(self))
+
+    def __repr__(self) -> str:
+        return '<PAD_STYLE: {}>'.format(self.__name)
+
+    def __str__(self) -> str:
+        return self.__name
+
+    def __eq__(self, other: typing.Any) -> bool:
+        if not isinstance(other, _PadStyle):
+            return False
+        return str(self) == str(other)
+
+
+PAD_STYLE_HASH1 = _PadStyle("HASH1")
+PAD_STYLE_HASH4 = _PadStyle("HASH4")
+PAD_STYLE_DEFAULT = PAD_STYLE_HASH4
+
+PAD_MAP = {
+    "#": {PAD_STYLE_HASH1: 1, PAD_STYLE_HASH4: 4},
+    "@": {PAD_STYLE_HASH1: 1, PAD_STYLE_HASH4: 1}
+}
+
+# Map width back to preferred padding character. Every pad style must have an
+# entry for width 1, which will also be the default pad character
+REVERSE_PAD_MAP = {
+    PAD_STYLE_HASH1: {1: "#"},
+    PAD_STYLE_HASH4: {1: "@", 4: "#"}
+}
+
+# Component of SPLIT_PATTERN (c.f FRANGE_PATTERN).
+_FRANGE_PART = r"""
+    -?\d+           # start frame
+    (?:             # optional range
+        -           #   range delimiter
+        -?\d+       #   end frame
+        (?:         #   optional stepping
+            [:xy]   #     step format
+            -?\d+   #     step value
+        )?
+    )?
+"""
+_FRANGE_PART = re.compile(_FRANGE_PART, re.X).pattern
+
+# Regular expression for matching a file sequence string.
+# Example: /film/shot/renders/bilbo_bty.1-100#.exr
+# Example: /film/shot/renders/bilbo_bty.1-100@.exr
+# Example: /film/shot/renders/bilbo_bty.1-100@@@@#.exr
+# Example: /film/shot/renders/bilbo_bty.1-100%04d.exr
+# Example: /film/shot/renders/bilbo_bty.1-100$F4.exr
+# Example: /film/shot/renders/bilbo_bty.1-100<UDIM>.exr
+SPLIT_PATTERN = r"""
+    ((?:{0}(?:,{0})*)?)     # range
+    (                       # pad format
+        [{1}]+              #   pad map characters
+        |
+        %\d*d               #   printf syntax pad format
+        |
+        \$F\d*              #   Houdini syntax pad format
+        |
+        <UDIM>|%\(UDIM\)d   #   UDIM Syntax pad format
+    )
+    """.format(_FRANGE_PART, ''.join(PAD_MAP))
+SPLIT_RE = re.compile(SPLIT_PATTERN, re.X)
+
+# Component of SPLIT_SUB_PATTERN (c.f FRANGE_PATTERN).
+# If both start and stop are present either both or neither should have
+# a fractional component to avoid ambiguity when basename ends in \d\.
+_FRANGE_SUB_PARTS = [
+    _FRANGE_PART,
+    r"""
+    (?:
+        -?\d+                     # start frame
+        (?:                       # optional range
+            -                     #   range delimiter
+            -?\d+                 #   end frame
+            (?:                   #   optional stepping
+                x                 #     step format
+                -?\d+\.\d+        #     step value
+            )?
+        )?
+    )
+    """,
+    r"""
+    (?:
+        -?\d+\.\d+                # start frame
+        (?:                       # optional range
+            -                     #   range delimiter
+            -?\d+\.\d+            #   end frame
+            (?:                   #   optional stepping
+                x                 #     step format
+                -?\d+(?:\.\d+)?   #     step value
+            )?
+        )?
+    )
+"""]
+_FRANGE_SUB_PARTS = [
+    re.compile(part, re.X).pattern for part in _FRANGE_SUB_PARTS
+]
+
+# Regular expression for matching a file sequence string allowing subframes.
+# Example: /film/shot/renders/bilbo_bty.1-100#.#.exr
+# Example: /film/shot/renders/bilbo_bty.1.5-2x0.1#.#.exr
+SPLIT_SUB_PATTERN = r"""
+    (                         # range
+        (?:
+            (?:{1}(?:,{1})*)
+            |
+            (?:{2}(?:,{2})*)
+            |
+            (?:{3}(?:,{3})*)
+        )?
+    )
+    (                         # pad format
+        [{0}]+(?:\.[{0}]+)?   #   pad map characters
+        |
+        %\d*d                 #   printf syntax pad format
+        |
+        \$F\d*                #   Houdini syntax pad format
+        |
+        <UDIM>|%\(UDIM\)d     #   UDIM Syntax pad format
+    )
+    """.format(''.join(PAD_MAP), *_FRANGE_SUB_PARTS)
+SPLIT_SUB_RE = re.compile(SPLIT_SUB_PATTERN, re.X)
+
+# Regular expression pattern for matching padding against a printf syntax
+# padding string E.g. %04d
+PRINTF_SYNTAX_PADDING_PATTERN = r"\A%(\d*)d\Z"
+PRINTF_SYNTAX_PADDING_RE = re.compile(PRINTF_SYNTAX_PADDING_PATTERN)
+
+# Regular expression pattern for matching padding against houdini syntax
+HOUDINI_SYNTAX_PADDING_PATTERN = r"\A\$F(\d*)\Z"
+HOUDINI_SYNTAX_PADDING_RE = re.compile(HOUDINI_SYNTAX_PADDING_PATTERN)
+
+# Legal patterns for UDIM style padding
+UDIM_PADDING_PATTERNS = ['<UDIM>', '%(UDIM)d']
+
+# Regular expression pattern for matching file names on disk.
+DISK_PATTERN = r"""
+    \A
+    ((?:.*[/\\])?)             # dirname
+    (.*?)                      # basename
+    (-?\d+)?                   # frame
+    (                          # ext
+        (?:\.\w*[a-zA-Z]\w?)*  #   optional leading alnum ext prefix (.foo.1bar)
+        (?:\.[^.]+)?           #   ext suffix
+    )
+    \Z
+    """
+DISK_RE = re.compile(DISK_PATTERN, re.X)
+
+# Regular expression pattern for matching file names on disk allowing subframes.
+DISK_SUB_PATTERN = r"""
+    \A
+    ((?:.*[/\\])?)             # dirname
+    (.*?)                      # basename
+    (-?\d+(?:\.\d+)?)?         # frame
+    (                          # ext
+        (?:\.\w*[a-zA-Z]\w?)*  #   optional leading alnum ext prefix (.foo.1bar)
+        (?:\.[^.]+)?           #   ext suffix
+    )
+    \Z
+    """
+DISK_SUB_RE = re.compile(DISK_SUB_PATTERN, re.X)
+
+# Regular expression pattern for matching frame set strings.
+# Examples: '1.0' or '1.0-100.0', '1.0-100.0x0.5', '1-100x0.25',
+# '1,2', etc.
+FRANGE_PATTERN = r"""
+    \A
+    (-?\d+(?:\.\d+)?)         # start frame
+    (?:                       # optional range
+        -                     #   range delimiter
+        (-?\d+(?:\.\d+)?)     #   end frame
+        (?:                   #   optional stepping
+            ([:xy])           #     step format
+            (-?\d+(?:\.\d+)?) #     step value
+        )?
+    )?
+    \Z
+    """
+FRANGE_RE = re.compile(FRANGE_PATTERN, re.X)
+
+# Regular expression for padding a frame range.
+PAD_PATTERN = r"""
+    (-?)(\d+(?:\.\d+)?)     # start frame
+    (?:                     # optional range
+        (-)                 #   range delimiter
+        (-?)(\d+(?:\.\d+)?) #   end frame
+        (?:                 #   optional stepping
+            ([:xy])         #     step format
+            (\d+(?:\.\d+)?) #     step value
+        )?
+    )?
+    """
+PAD_RE = re.compile(PAD_PATTERN, re.X)
