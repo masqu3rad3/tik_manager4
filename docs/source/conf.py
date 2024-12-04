677914a7c18fd726a87d897ee1065b5775f007b6
Arda Kutlu <ardakutlu@gmail.com>
2024-12-04 11:27:36 +0000

    tweaking the auto documentation configuration to exclude external libraries

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,13 +1,23 @@
 # Configuration file for the Sphinx documentation builder.
 
+import os
+import sys
+
+# Add the path to the tik_manager4 folder to sys.path
+sys.path.insert(0, os.path.abspath('../../tik_manager4/'))
+
+# Import the version from _version.py
+from _version import __version__
+
 # -- Project information
 
 project = 'Tik Manager4'
 copyright = '2023, Arda Kutlu'
 author = 'Arda Kutlu'
 
-release = '0.1'
-version = '4.0.2'
+# Automatically populate version and release from _version.py
+version = __version__  # Short X.Y version
+release = __version__  # Full version including alpha/beta/rc tags
 
 # -- General configuration
 
@@ -23,7 +33,7 @@ extensions = [
 
 autoapi_dirs = ['../../tik_manager4/']
 autoapi_type = 'python'
-autoapi_ignore = ['*setup*', '*shiboken*', '*PySide2*', '*PySide6*', '*PyQt5*', '*PyQt6*']
+autoapi_ignore = ['*setup*', '*shiboken*', '*PySide2*', '*PySide6*', '*PyQt5*', '*PyQt6*', '*external/**/*']
 autoapi_file_patterns = ['*.py']
 add_module_names = False
 autoapi_member_order = 'groupwise'
