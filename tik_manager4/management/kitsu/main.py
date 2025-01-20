59340384348040d21ec9dbd5e418a0ce67743b5c
Arda Kutlu <ardakutlu@gmail.com>
2025-01-20 00:44:15 +0000

    kitsu authentication wip

--- tik_manager4/management/kitsu/main.py
+++ tik_manager4/management/kitsu/main.py
@@ -2,7 +2,7 @@
 import importlib
 import logging
 import sys
-from importlib.metadata import metadata
+import os
 from pathlib import Path
 from copy import deepcopy
 from datetime import datetime
@@ -12,6 +12,8 @@ from tik_manager4.management.management_core import ManagementCore
 from tik_manager4.management.enums import EventType
 from tik_manager4.management.exceptions import AuthenticationError, SyncError
 
+from tik_manager4.management.kitsu.ui import login
+
 
 external_folder = Path(__file__).parents[2] / "external"
 
@@ -20,8 +22,8 @@ kitsu_folder = (external_folder / "kitsu").as_posix()
 if kitsu_folder not in sys.path:
     sys.path.append(kitsu_folder)
 
-# import gazu
 from requests.exceptions import ConnectionError
+from gazu.exception import NotAuthenticatedException
 
 logging.basicConfig(level=logging.INFO)
 LOG = logging.getLogger(__name__)
@@ -63,12 +65,16 @@ class ProductionPlatform(ManagementCore):
         """Authenticate the user."""
         self.gazu = importlib.import_module("gazu")
         self.gazu.set_host(self.host_api)
+        os.environ["CGWIRE_HOST"] = self.host_api
         # FIXME: This is a temporary solution. The user should be able to login
         try:
-            self.gazu.log_in("admin@example.com", "mysecretpassword")
+            login_widget = login.Login(self.gazu)
+            ret = login_widget.exec_()
+            if not ret:
+                return None, "Canceled by user."
+            # self.gazu.log_in("admin@example.com", "mysecretpassword")
         except ConnectionError as e:
             return None, f"Connection Error: {e}"
-
 
         # TODO: Test if the gazu is authenticated
         self.is_authenticated = True
@@ -127,151 +133,6 @@ class ProductionPlatform(ManagementCore):
                 shot_categories.append(name)
 
         return asset_categories, shot_categories
-
-    # def _get_asset_categories(self):
-    #     """Collect the asset task types from kitsu."""
-    #     return self.gazu.task.all_task_types_for_asset()
-    #
-    # def _get_shot_categories(self):
-    #     """Collect the steps for shots from the Shotgrid server."""
-    #     shot_categories = [
-    #         x["code"]
-    #         for x in
-    #         self.sg.find("Step", [["entity_type", "is", "Shot"]], ["code"])
-    #     ]
-    #     return shot_categories
-
-    # def _sync_new_asset(self, asset_data, assets_sub, asset_categories):
-    #     """Sync a new asset from Shotgrid.
-    #
-    #     Args:
-    #         asset_data (dict): The data of the asset.
-    #             Must contain the keys "id", "name", and "entity_type_id".
-    #         assets_sub (SubProject): The sub project to add the asset to.
-    #         asset_categories (list): The categories of the asset.
-    #     """
-    #     asset_id = asset_data["id"]
-    #     asset_name = asset_data["name"]
-    #     asset_type_dict = self.gazu.entity.get_entity_type(asset_data["entity_type_id"])
-    #     asset_type = asset_type_dict.get("name", None)
-    #     if asset_type:
-    #         if assets_sub.subs.get(asset_type) is None:
-    #             sub = self.tik_main.project.create_sub_project(
-    #                 asset_type, parent_path="Assets"
-    #             )
-    #         else:
-    #             sub = self.tik_main.project.subs["Assets"].subs[asset_type]
-    #     else:
-    #         sub = assets_sub
-    #     asset_name = utils.sanitize_text(asset_name)
-    #     task = sub.add_task(asset_name, categories=asset_categories, uid=asset_id)
-    #     if asset_data["canceled"]:
-    #         task.omit()
-    #
-    #     return task
-    #
-    # def _sync_new_shot(self, shot_data, shots_sub, shot_categories):
-    #     """Sync a new shot from Shotgrid.
-    #
-    #     Args:
-    #         shot_data (dict): The data of the shot.
-    #             Must contain the keys "id", "code", "sg_sequence", and "episode".
-    #         shots_sub (SubProject): The sub project to add the shot to.
-    #         shot_categories (list): The categories of the shot.
-    #     """
-    #     shot_id = shot_data["id"]
-    #     shot_name = shot_data["name"]
-    #     sequence = self.gazu.entity.get_entity(shot_data["parent_id"])
-    #     is_episodic = sequence.get("parent_id", None)
-    #     query_sub = shots_sub
-    #     if is_episodic:
-    #         episode_data = self.gazu.entity.get_entity(sequence["parent_id"])
-    #         episode = episode_data["name"]
-    #         if query_sub.subs.get(episode) is None:
-    #             query_sub = self.tik_main.project.create_sub_project(
-    #                 episode,
-    #                 parent_path=query_sub.path,
-    #                 uid=episode_data["id"],
-    #                 mode="episode",
-    #             )
-    #         else:
-    #             query_sub = query_sub.subs[episode]
-    #
-    #     # looks like kitsu requires a sequence for all shots
-    #     if query_sub.subs.get(sequence["name"]) is None:
-    #         query_sub = self.tik_main.project.create_sub_project(
-    #             sequence["name"],
-    #             parent_path=query_sub.path,
-    #             uid=sequence["id"],
-    #         )
-    #     else:
-    #         query_sub = shots_sub.subs[sequence["name"]]
-    #     shot_name = utils.sanitize_text(shot_name)
-    #
-    #
-    #     metadata_overrides = self._retrieve_metadata_overrides(shot_data.get("data"))
-    #
-    #     task = query_sub.add_task(
-    #         shot_name,
-    #         categories=shot_categories,
-    #         uid=shot_id,
-    #         metadata_overrides=metadata_overrides,
-    #     )
-    #     if shot_data["canceled"]:
-    #         task.omit()
-    #
-    #     return task
-
-    # def _sync_asset(self, asset_id):
-    #     """"Sync the properties of the asset with the one in Kitsu Server.
-    #
-    #     This function assumes that the asset is already in the tik project.
-    #     """
-    #     # find the asset in the tik project
-    #     kitsu_asset = self.gazu.asset.get_asset(asset_id)
-    #     tik_task = self.tik_main.project.find_task_by_uid(asset_id)
-    #
-    #     if kitsu_asset["canceled"]:
-    #         tik_task.omit()
-    #     else:
-    #         tik_task.revive()
-    #
-    # def _sync_shot(self, shot_id):
-    #     """Sync the properties of the shot with the one in Kitsu Server.
-    #
-    #     This function assumes that the shot is already in the tik project.
-    #     """
-    #     kitsu_shot = self.gazu.shot.get_shot(shot_id)
-    #     tik_task = self.tik_main.project.find_task_by_uid(shot_id)
-    #
-    #     if kitsu_shot["canceled"]:
-    #         tik_task.omit()
-    #     else:
-    #         tik_task.revive()
-    #
-    #     metadata_overrides = self._retrieve_metadata_overrides(kitsu_shot.get("data"))
-    #
-    #     for key, value in metadata_overrides.items():
-    #         tik_meta_key = self.metadata_pairing.get(key)
-    #         if not tik_meta_key:
-    #             continue
-    #         if value:
-    #             tik_task._metadata_overrides[tik_meta_key] = value
-    #         elif tik_task._metadata_overrides.get(tik_meta_key):
-    #             tik_task._metadata_overrides.pop(tik_meta_key)
-    #
-    #     tik_task.edit_property("metadata_overrides", tik_task._metadata_overrides)
-    #     tik_task.apply_settings()
-
-    # def _retrieve_metadata_overrides(self, data):
-    #     """Retrieve the metadata overrides from the data."""
-    #     if not data:
-    #         return {}
-    #     metadata_overrides = {}
-    #     for key, value in data.items():
-    #         if key in self.metadata_pairing:
-    #             metadata_overrides[self.metadata_pairing[key]] = value
-    #     return metadata_overrides
 
     def create_from_project(self, project_root, kitsu_project_id, set_project=True):
         """Create a tik manager project from a kitsu project."""
@@ -406,10 +267,6 @@ class ProductionPlatform(ManagementCore):
         if not project:
             raise SyncError(f"Project ID {project_id} not found in Kitsu host.")
 
-        # changes = self._get_changes_from_log(project)
-        # if not changes:
-        #     return None
-
         asset_categories, shot_categories = self._get_categories(project)
 
         assets_sub = self._get_assets_sub()
@@ -423,125 +280,14 @@ class ProductionPlatform(ManagementCore):
                                             )
 
         for sync_block in sync_blocks:
-            print(sync_block)
-            print(sync_block.event_type)
-            # sync_block.execute()
-
-        # # Get the last sync date
-        # last_sync = self.tik_main.project.settings.get("last_sync")
-        # parsed_time = datetime.strptime(last_sync, "%Y-%m-%dT%H:%M:%SZ")
-        # formatted_time = parsed_time.strftime("%Y-%m-%dT%H:%M:%S")
-        # events = self.gazu.sync.get_last_events(project=project,
-        #                                         after=formatted_time)
-        #
-        # valid_event_types = ["asset:new", "shot:new", "asset:delete", "shot:delete",
-        #                      "asset:update", "shot:update"]
-        # from pprint import pprint
-        # # pprint(events)
-        # for event in reversed(events):
-        #     event_type = event.get("name")
-        #     if event_type not in valid_event_types:
-        #         continue
-        #     sync_block = SyncBlock(self.gazu, self.tik_main, project)
-        #     if event_type == "asset:new":
-        #         sync_block.event_type = EventType.NEW_ASSET
-        #         asset_data = self.gazu.asset.get_asset(event["data"]["asset_id"])
-        #         sync_block.kitsu_data = asset_data
-        #         sync_block.subproject = assets_sub
-        #         sync_block.categories = asset_categories
-        #         pprint(asset_data)
-        #     elif event_type == "shot:new":
-        #         sync_block.event_type = EventType.NEW_SHOT
-        #         shot_data = self.gazu.shot.get_shot(event["data"]["shot_id"])
-        #         sync_block.kitsu_data = shot_data
-        #         sync_block.subproject = shots_sub
-        #         sync_block.categories = shot_categories
-        #     elif event_type in ["asset:delete", "asset:update"]:
-        #         sync_block.event_type = EventType.UPDATE_ASSET
-        #         asset_data = self.gazu.asset.get_asset(event["data"]["asset_id"])
-        #         sync_block.kitsu_data = asset_data
-        #     elif event_type in ["shot:delete", "shot:update"]:
-        #         sync_block.event_type = EventType.UPDATE_SHOT
-        #         shot_data = self.gazu.shot.get_shot(event["data"]["shot_id"])
-        #         sync_block.kitsu_data = shot_data
-            # sync_block.execute()
-
-
-        # raise SyncError("Syncing is not implemented yet.")
-
-        # for data_dict in changes:
-        #     sync_block = SyncBlock(self.gazu, self.tik_main, project)
-        #
-        #     action = data_dict.get("action")
-        #     if action == EventType.NEW_ASSET:
-        #         self._sync_new_asset(data_dict.get("data"))
-        #     elif action == EventType.NEW_SHOT:
-        #         self._sync_new_shot(data_dict.get("data"))
-        #     elif action == EventType.UPDATE_ASSET:
-        #         self._sync_asset(data_dict.get("data"))
-        #     elif action == EventType.UPDATE_SHOT:
-        #         self._sync_shot(data_dict.get("data"))
+            sync_block.execute()
 
         # Update the last sync date
-        # TODO: DISABLED FOR TESTING
-        # self.tik_main.project.settings.edit_property("last_sync", self.date_stamp())
-        # self.tik_main.project.settings.apply_settings(force=True)
+        self.tik_main.project.settings.edit_property("last_sync", self.date_stamp())
+        self.tik_main.project.settings.apply_settings(force=True)
 
         return sync_blocks
 
-
-    def _get_changes_from_log(self, project):
-        """Get the changes from the log."""
-        # project_id = self.tik_main.project.settings.get("host_project_id")
-        # if not project_id:
-        #     raise SyncError("Project is not linked to a Kitsu project.")
-        # project = self.gazu.project.get_project(project_id)
-        # if not project:
-        #     raise SyncError(f"Project ID {project_id} not found in Kitsu host.")
-
-
-        # Get the last sync date
-        last_sync = self.tik_main.project.settings.get("last_sync")
-
-        parsed_time = datetime.strptime(last_sync,"%Y-%m-%dT%H:%M:%SZ")
-        formatted_time = parsed_time.strftime("%Y-%m-%dT%H:%M:%S")
-        events = self.gazu.sync.get_last_events(project=project, after=formatted_time)
-
-        # Initialize an empty list to hold event information
-        event_list = []
-
-        # Lists to hold IDs for additional data queries
-        asset_ids = []
-        shot_ids = []
-
-        valid_event_types = ["asset:new", "shot:new", "asset:delete", "shot:delete",
-                             "asset:update", "shot:update"]
-        for event in events:
-            action = None
-
-            event_type = event.get("name")
-            if event_type == "asset:new":
-                action = EventType.NEW_ASSET
-                asset_ids.append(event["data"]["asset_id"])
-            elif event_type == "shot:new":
-                action = EventType.NEW_SHOT
-                shot_ids.append(event["data"]["shot_id"])
-            elif event_type == "asset:delete":
-                action = EventType.DELETE_ASSET
-                asset_ids.append(event["data"]["asset_id"])
-            elif event_type == "shot:delete":
-                action = EventType.DELETE_SHOT
-                shot_ids.append(event["data"]["shot_id"])
-            elif event_type == "asset:update":
-                action = EventType.UPDATE_ASSET
-                asset_ids.append(event["data"]["asset_id"])
-            elif event_type == "shot:update":
-                action = EventType.UPDATE_SHOT
-                asset_ids.append(event["data"]["shot_id"])
-
-        # print("last_sync", last_sync)
-
-        return event_list
 
     @staticmethod
     def get_settings_ui():
@@ -717,7 +463,7 @@ class SyncBlock:
         This function assumes that the asset is already in the tik project.
         """
         asset_id = self.kitsu_data["id"]
-        tik_task = self.tik_main.project.find_task_by_uid(asset_id)
+        tik_task = self.tik_main.project.find_task_by_id(asset_id)
 
         if self.kitsu_data["canceled"]:
             tik_task.omit()
@@ -730,7 +476,7 @@ class SyncBlock:
         This function assumes that the shot is already in the tik project.
         """
         shot_id = self.kitsu_data["id"]
-        tik_task = self.tik_main.project.find_task_by_uid(shot_id)
+        tik_task = self.tik_main.project.find_task_by_id(shot_id)
 
         if self.kitsu_data["canceled"]:
             tik_task.omit()
@@ -740,13 +486,10 @@ class SyncBlock:
         metadata_overrides = self._retrieve_metadata_overrides(self.kitsu_data.get("data"))
 
         for key, value in metadata_overrides.items():
-            tik_meta_key = self.metadata_pairing.get(key)
-            if not tik_meta_key:
-                continue
             if value:
-                tik_task._metadata_overrides[tik_meta_key] = value
-            elif tik_task._metadata_overrides.get(tik_meta_key):
-                tik_task._metadata_overrides.pop(tik_meta_key)
+                tik_task._metadata_overrides[key] = value
+            elif tik_task._metadata_overrides.get(key):
+                tik_task._metadata_overrides.pop(key)
 
         tik_task.edit_property("metadata_overrides", tik_task._metadata_overrides)
         tik_task.apply_settings()
@@ -757,14 +500,9 @@ class SyncBlock:
             return {}
         metadata_overrides = {}
         for key, value in data.items():
-            if key in self.metadata_pairing:
+
+            if key in self.metadata_pairing.keys():
                 metadata_overrides[self.metadata_pairing[key]] = value
+
         return metadata_overrides
-
-
-# gazu.set_host("http://localhost/api")
-# gazu.log_in("admin@example.com", "mysecretpassword")
-#
-# projects = gazu.project.all_open_projects()
-# print("projects", projects)
 
---
+++ tik_manager4/management/kitsu/ui/login.py
@@ -0,0 +1,216 @@
+"""Credit to https://github.com/Colorbleed for the original code.
+This is a modified version of the original code."""
+
+import os
+import logging
+import re
+
+# import gazu
+
+from tik_manager4.ui.Qt import QtWidgets, QtGui, QtCore
+from tik_manager4.ui import pick
+
+
+LOG = logging.getLogger(__name__)
+
+class AnimatedLabel(QtWidgets.QLabel):
+    """
+    QLabel with animated background color.
+    """
+
+    def __init__(self):
+        super(AnimatedLabel, self).__init__()
+        self.setStyleSheet(
+            """
+            background-color: #CC4444;
+            color: #F5F5F5;
+            padding: 5px;
+            """
+        )
+        self.setWordWrap(True)
+        self.create_animation()
+
+    def create_animation(self):
+        """
+        Create the animation of the color background.
+        """
+        color_begin = QtGui.QColor("#943434")
+        color_end = QtGui.QColor("#CC4444")
+        self.color_anim = QtCore.QPropertyAnimation(self, b"background_color")
+        self.color_anim.setStartValue(color_begin)
+        self.color_anim.setEndValue(color_end)
+        self.color_anim.setDuration(400)
+
+    def start_animation(self):
+        """
+        Start the animation of the color background.
+        """
+        self.color_anim.stop()
+        self.color_anim.start()
+
+    def get_back_color(self):
+        """
+        Get the background color.
+        """
+        return self.palette().color(QtGui.QPalette.Window)
+
+    def set_back_color(self, color):
+        """
+        Set the given color as background color by parsing the style sheet.
+        """
+        style = self.styleSheet()
+        pattern = "background-color:[^\n;]*"
+        new = "background-color: %s" % color.name()
+        style = re.sub(pattern, new, style, flags=re.MULTILINE)
+        self.setStyleSheet(style)
+
+    # Property to animate : the label background color
+    background_color = QtCore.Property(
+        QtGui.QColor, get_back_color, set_back_color
+    )
+
+
+class Login(QtWidgets.QDialog):
+    """Log-in dialog to CG-Wire"""
+
+    logged_in = QtCore.Signal(bool)
+
+    def __init__(self, gazu_instance, parent=None, initialize_host=True):
+        super(Login, self).__init__(parent)
+        self.gazu = gazu_instance
+
+        self.setWindowTitle("Connect to Kitsu")
+
+        # Kitsu logo
+        logo_label = QtWidgets.QLabel()
+        pixmap = pick.pixmap("logo_kitsu.png")
+        logo_label.setPixmap(pixmap)
+        logo_label.setAlignment(QtCore.Qt.AlignCenter)
+
+        form = QtWidgets.QFormLayout()
+        form.setContentsMargins(10, 15, 10, 5)
+        form.setObjectName("form")
+
+        # Host
+        host_label = QtWidgets.QLabel("Kitsu URL:")
+        host_input = QtWidgets.QLineEdit()
+        host_input.setPlaceholderText("https://xxx.cg-wire.com/api")
+
+        # User
+        user_label = QtWidgets.QLabel("Username:")
+        user_input = QtWidgets.QLineEdit()
+        user_input.setPlaceholderText("user@host.com")
+
+        # Password
+        password_label = QtWidgets.QLabel("Password:")
+        password_input = QtWidgets.QLineEdit()
+        password_input.setEchoMode(QtWidgets.QLineEdit.Password)
+
+        # Error
+        error = AnimatedLabel()
+        error.hide()
+
+        # Buttons
+        login = QtWidgets.QPushButton("Login")
+        login.setAutoDefault(True)
+        login.setDefault(True)
+        buttons = QtWidgets.QHBoxLayout()
+        buttons.addWidget(login)
+
+        form.addRow(host_label, host_input)
+        form.addRow(user_label, user_input)
+        form.addRow(password_label, password_input)
+
+        self.inputs = dict()
+        self.inputs["host"] = host_input
+        self.inputs["user"] = user_input
+        self.inputs["password"] = password_input
+        self.error = error
+
+        layout = QtWidgets.QVBoxLayout(self)
+        layout.addWidget(logo_label)
+        layout.addLayout(form)
+        layout.addWidget(error)
+        layout.addLayout(buttons)
+
+        self.resize(325, 160)
+
+        # Connections
+        login.clicked.connect(self.on_login)
+
+        if initialize_host:
+            # Automatically enter host if available.
+            self.initialize_host()
+
+    def initialize_host(self):
+        """Initialize host value based on environment"""
+
+        host_input = self.inputs["host"]
+
+        host = os.environ.get("CGWIRE_HOST", None)
+        if host is None:
+            gazu_host = self.gazu.client.get_host()
+            if gazu_host != "http://gazu.change.serverhost/api":
+                # Assume the host in gazu.client is already set correctly
+                # and copy it into these settings.
+                LOG.debug(
+                    "Setting CG-Wire host from gazu.client: %s" % gazu_host
+                )
+                host = gazu_host
+        else:
+            LOG.debug(
+                "Setting CG-Wire host from environment "
+                "variable CGWIRE_HOST: %s" % host
+            )
+
+        if host:
+            # Force the host by environment variable
+            host_input.setText(host)
+            host_input.setEnabled(False)
+        else:
+            host_input.setEnabled(True)
+
+    def on_login(self):
+        """Perform login with current settings in the dialog."""
+
+        host = self.inputs["host"].text()
+        user = self.inputs["user"].text()
+        password = self.inputs["password"].text()
+
+        try:
+            self.gazu.set_host(host)
+            if not self.gazu.client.host_is_up():
+                raise ConnectionError(
+                    "Could not connect to the server. Is the host URL correct?"
+                )
+            result = self.gazu.log_in(user, password)
+        except Exception as exc:
+            message = str(exc)
+            if message.startswith("auth/login"):
+                message = (
+                    "Could not connect to the server. Is the host URL correct?"
+                )
+            if message.startswith("('auth/login',"):
+                # Failed to login
+                message = (
+                    "Login verification failed.\n"
+                    "Please ensure your username and "
+                    "password are correct."
+                )
+            else:
+                # Something else happened.
+                # For readability produce new line
+                # around every dot with following space
+                message = message.replace(". ", ".\n")
+
+            self.error.setText(message)
+            self.error.show()
+            self.error.start_animation()
+            self.logged_in.emit(False)
+            return
+
+        if result:
+            name = "{user[first_name]} {user[last_name]}".format(**result)
+            LOG.info("Logged in as %s.." % name)
+            self.logged_in.emit(True)
+        self.accept()
